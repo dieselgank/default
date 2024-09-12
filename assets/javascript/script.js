4dcf025ac80067601afb339e15e1feb420adfdbc
dieselgank <adamraya11@gmail.com>
2024-09-12 20:31:18 +0700

    tes1

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -79,3 +79,37 @@ fetch("https://ipapi.co/json/")
 // Mendapatkan bahasa browser pengguna
 const browserLanguage = navigator.language || navigator.userLanguage;
 document.getElementById("browser-language").textContent = browserLanguage;
+
+function detectBrowser() {
+  const userAgent = navigator.userAgent;
+
+  let browserName;
+  let browserVersion;
+
+  if (userAgent.indexOf("Firefox") > -1) {
+    browserName = "Mozilla Firefox";
+    browserVersion = userAgent.match(/Firefox\/(\d+\.\d+)/)[1];
+  } else if (userAgent.indexOf("Chrome") > -1) {
+    browserName = "Google Chrome";
+    browserVersion = userAgent.match(/Chrome\/(\d+\.\d+)/)[1];
+  } else if (userAgent.indexOf("Safari") > -1) {
+    browserName = "Apple Safari";
+    browserVersion = userAgent.match(/Version\/(\d+\.\d+)/)[1];
+  } else if (userAgent.indexOf("Opera") > -1 || userAgent.indexOf("OPR") > -1) {
+    browserName = "Opera";
+    browserVersion =
+      userAgent.match(/Opera\/(\d+\.\d+)|OPR\/(\d+\.\d+)/)[1] ||
+      userAgent.match(/OPR\/(\d+\.\d+)/)[1];
+  } else if (userAgent.indexOf("MSIE") > -1 || !!document.documentMode) {
+    browserName = "Internet Explorer";
+    browserVersion = userAgent.match(/MSIE (\d+\.\d+)/)[1];
+  } else {
+    browserName = "Unknown";
+    browserVersion = "Unknown";
+  }
+
+  document.getElementById("browser").textContent = browserName;
+  document.getElementById("version").textContent = browserVersion;
+}
+
+detectBrowser();
